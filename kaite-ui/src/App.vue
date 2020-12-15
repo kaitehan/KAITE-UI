eda268b4e3df8ddd4bbbc3f7e7cd8349b0b6b057
kaite_han@163.com <kaite_han@163.com>
2020-12-15 15:00:21 +0800

    初始化项目

---
+++ kaite-ui/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      hi
+    </div>
+     <Frank/>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
