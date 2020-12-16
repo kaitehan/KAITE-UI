51f62db084ae4421bf6203f58c9f99ad2f962b50
kaite_han@163.com <kaite_han@163.com>
2020-12-16 10:51:13 +0800

    Home样式更新

--- kaite-ui/src/views/Home.vue
+++ kaite-ui/src/views/Home.vue
@@ -15,7 +15,28 @@
 <script type="ts">
 import Topnav from '../components/Topnav.vue'
 export default {
-  components:{Topnav}
+  components:{ Topnav }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
