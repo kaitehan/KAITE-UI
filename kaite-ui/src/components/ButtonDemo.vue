5f2b0fddbbf2f46c19a052d4b85d48d64ec66462
kaite_han@163.com <kaite_han@163.com>
2020-12-18 19:03:01 +0800

    Button添加theme属性

--- kaite-ui/src/components/ButtonDemo.vue
+++ kaite-ui/src/components/ButtonDemo.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>BUTTON 文档</div>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好 </Button>
+    <Button theme="button">你好 </Button>
+    <Button theme="link">你好 </Button>
+    <Button theme="text">你好 </Button>
+  </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log(111);
+    };
+    return { onClick };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ kaite-ui/src/lib/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="kaite-button" :class="{ [`theme-${theme}`]: theme }">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+  },
+
+  setup(props, context) {
+    const { ...rest } = context.attrs;
+    return { rest };
+  },
+};
+</script>
+<style lang="scss"></style>
