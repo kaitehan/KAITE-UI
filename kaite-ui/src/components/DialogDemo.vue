1bfcfc05dfc4c89f548616122a5089018a3f0791
kaite_han@163.com <kaite_han@163.com>
2020-12-19 15:44:39 +0800

    优化代码结构

--- kaite-ui/src/components/DialogDemo.vue
+++ kaite-ui/src/components/DialogDemo.vue
@@ -1,25 +1,24 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例 1</h1>
-  <div style="position: relative; z-index: 1">
-    <Button @click="toggle">toggle</Button>
-    <Dialog
-      v-model:visible="x"
-      :closeOnClickOverlay="false"
-      :ok="fn1"
-      :cancel="fn2"
-    >
-      <template v-slot:content>
-        <strong>hi</strong>
-        <div>hi2</div>
-      </template>
-      <template v-slot:title>
-        <strong>加粗的标题</strong>
-      </template>
-    </Dialog>
-    <h1>示例 2</h1>
-    <Button @click="showDialog">show</Button>
-  </div>
+  <Button @click="toggle">toggle</Button>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="fn1"
+    :cancel="fn2"
+  >
+    <template v-slot:content>
+      <strong>hi</strong>
+      <div>hi2</div>
+    </template>
+    <template v-slot:title>
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
+
+  <h1>示例 2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
