0395245cefe5f7e22aebed8b8f7f1b511854d68b
kaite_han@163.com <kaite_han@163.com>
2020-12-22 12:19:55 +0800

    使用本地svg.js以及实现圆弧

--- kaite-ui/src/main.ts
+++ kaite-ui/src/main.ts
@@ -5,6 +5,8 @@ import App from "./App.vue";
 
 import { router } from "./router";
 
+import "./lib/svg.js";
+
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
--- kaite-ui/src/views/Home.vue
+++ kaite-ui/src/views/Home.vue
@@ -38,6 +38,7 @@ export default {
     rgba(227, 255, 253, 1) 0%,
     rgba(183, 233, 230, 1) 100%
   );
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features {
   > svg {
