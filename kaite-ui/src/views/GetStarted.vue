b46367479f8a5c6e11dc5fb803924ca323719518
kaite_han@163.com <kaite_han@163.com>
2020-12-24 13:45:51 +0800

    填充文档

--- kaite-ui/src/views/GetStarted.vue
+++ kaite-ui/src/views/GetStarted.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>开始</div>
+  <article>
+    <h1>开始使用</h1>
+
+    <p>请先<a href="#/doc/install">安装</a>本组件库。</p>
+    <p>然后在你的代码中写入下面的代码</p>
+    <pre><code>import {Button, Tabs, Switch, Dialog} from "king-ui"</code></pre>
+    就可以使用我提供的组件了。
+
+    <h2>Vue 单文件组件</h2>
+    <p>代码示例：</p>
+    <pre><code>&lt;template&gt;
+  &lt;div&gt;&lt;Button&gt;按钮&lt;/Button&gt;&lt;/div&gt;
+&lt;/template&gt;
+&lt;script&gt;
+  import {Button, Tabs, Switch, Dialog} from "king-ui"
+  export default {
+    components: {Button}
+  }
+&lt;/script&gt;
+ </code></pre>
+  </article>
 </template>
--- kaite-ui/src/views/Install.vue
+++ kaite-ui/src/views/Install.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>z安装</div>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install kaite-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add kaite-ui</code></pre>
+
+    <p>下一节：<a href="#/doc/get-started">开始使用</a></p>
+  </article>
 </template>
