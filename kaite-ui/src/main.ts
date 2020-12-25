960405d307124dcc693c4592f34803a780ca3d13
kaite_han@163.com <kaite_han@163.com>
2020-12-25 19:00:52 +0800

    完善代码，实现异步加载md文件

--- kaite-ui/src/main.ts
+++ kaite-ui/src/main.ts
@@ -8,6 +8,9 @@ import { router } from "./router";
 import "./lib/svg.js";
 import "github-markdown-css";
 
+import Markdown from "./components/Markdown.vue";
+
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+app.component("Markdown", Markdown);
--- kaite-ui/src/router.ts
+++ kaite-ui/src/router.ts
@@ -7,10 +7,9 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocFileDemo from "./components/DocFileDemo.vue";
+import Markdown from "./components/Markdown.vue";
 
-import Intro from "./views/Intro.vue";
-import GetStarted from "./views/GetStarted.vue";
-import Install from "./views/Install.vue";
+import { h } from "vue";
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -22,9 +21,21 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocFileDemo },
-        { path: "intro", component: Intro },
-        { path: "get-started", component: GetStarted },
-        { path: "install", component: Install },
+        {
+          path: "intro",
+          component: h(Markdown, { path: "../markdown/intro.md", key: "1" }),
+        },
+        {
+          path: "get-started",
+          component: h(Markdown, {
+            path: "../markdown/get-started.md",
+            key: "2",
+          }),
+        },
+        {
+          path: "install",
+          component: h(Markdown, { path: "../markdown/install.md", key: "3" }),
+        },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
