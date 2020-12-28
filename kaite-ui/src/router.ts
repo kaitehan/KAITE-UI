b5975d1cbc01d158c4352bd4c83f1cd79cdf9822
kaite_han@163.com <kaite_han@163.com>
2020-12-28 08:56:45 +0800

    优化代码

--- kaite-ui/src/router.ts
+++ kaite-ui/src/router.ts
@@ -7,9 +7,6 @@ import ButtonDemo from "./components/ButtonPage/ButtonDemo.vue";
 import DialogDemo from "./components/DialogPage/DialogDemo.vue";
 import TabsDemo from "./components/TabsPage/TabsDemo.vue";
 
-// import Intro from "./views/Intro.vue";
-// import Install from "./views/Install.vue";
-// import Getstarted from "./views/Getstarted.vue";
 import Intro from "./markdown/intro.md";
 import Install from "./markdown/install.md";
 import Getstarted from "./markdown/get-started.md";
